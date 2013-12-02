f55909171ed4bdb3c0369b0dfb97a759750e9846
jeffreybarneveld <jsbarneveld@gmail.com>
2013-12-02 10:43:21 +0100

    update

--- www/js/testapp.js
+++ www/js/testapp.js
@@ -747,7 +747,7 @@ var App = (function() {
 		   usedatum = d.getFullYear();
 		   if ((d.getMonth()+1)<10) { usedatum="0"+(d.getMonth()+1)+'-'+usedatum; } else { usedatum=(d.getMonth()+1)+'-'+usedatum;} 
 		   if (d.getDate()<10)  { usedatum="0"+d.getDate()+'-'+usedatum; } else { usedatum=d.getDate()+'-'+usedatum;} 
-		   myhtml+='<td style="width:30px;height:30px;"><input type=button id="datumknop'+idx+'" style="width:30px !important;height:30px !important;border:solid 1px #999999;margin:0px;" value="'+d.getDate()+'" onclick="alert(\'check1\');DatumselectKnopActie(\''+usedatum+'\');alert(\'check2\');"></td>';
+		   myhtml+='<td style="width:30px;height:30px;"><input type=button id="datumknop'+idx+'" style="width:30px !important;height:30px !important;border:solid 1px #999999;margin:0px;" value="'+d.getDate()+'" onclick="alert(\'checkA\');alert(\'checkB\');DatumselectKnopActie(\''+usedatum+'\');alert(\'checkC\');"></td>';
 		   if (curdag==7) { myhtml+='</tr>'; curdag=0; } //na de laatste dag rij afsluiten
 
 		   curts += 24*3600*1000; //dag verder	
